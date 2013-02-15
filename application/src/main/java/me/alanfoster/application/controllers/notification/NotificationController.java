44bed81c1f75e95d08208aa932c47068a8b3fa39
Alan Foster <alan@alanfoster.me>
2013-02-15 00:33:31 +0000

    Add nicer user interface for 404 and exception handling

--- application/src/main/java/me/alanfoster/application/controllers/notification/NotificationController.java
+++ application/src/main/java/me/alanfoster/application/controllers/notification/NotificationController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
- * A class for handling generic form response notifications
+ * A class for handling generic notifications
  * IE for showing the user success/failure of forms.
- *
+ * <p/>
  * This class will take advantage of the POST/REDIRECT/GET pattern
  * by using FlashAttributes
  *
@@ -23,10 +23,34 @@ public class NotificationController {
      * Shows basic form result notifications.
      * Note, this relies on Flash attributes being provided
      * by the original sender
+     *
      * @return the required model to render
      */
     @RequestMapping(value = "/formResult")
-    public final String searchEmployee() {
+    public final String formResultNotification() {
         return NotificationModelConfig.Notification.getModelName();
     }
+
+    /**
+     * Shows basic 404 notifications.
+     * by the original sender
+     *
+     * @return the required model to render
+     */
+    @RequestMapping(value = "/404")
+    public final String pageNotFoundNotification() {
+        return NotificationModelConfig.Notification.getModelName();
+    }
+
+    /**
+     * Shows basic exception notifications.
+     * by the original sender
+     *
+     * @return the required model to render
+     */
+    @RequestMapping(value = "/error")
+    public final String errorNotification() {
+        return NotificationModelConfig.Notification.getModelName();
+    }
+
 }
